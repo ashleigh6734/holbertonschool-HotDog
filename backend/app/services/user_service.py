c72d95f070d71ed845f6b36916324e3123a79c9c
Emily Chew <emilychewsh@gmail.com>
2026-02-03 17:33:17 +1100

    add delete user and get all users function

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -71,3 +71,16 @@ class UserService():
         db.session.commit()
         return user
     
+    @staticmethod
+    def delete_user(user_id):
+        user = User.query.get(user_id)
+        if not user:
+            return None
+        
+        db.session.delete(user)
+        db.session.commit()
+        return user
+    
+    @staticmethod
+    def get_all_users():
+        return User.query.all()
