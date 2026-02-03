2f21ccf975719cd9710c6e5d1d9d9d2614c44bd9
Emily Chew <emilychewsh@gmail.com>
2026-02-03 17:02:41 +1100

    Add UserService and create user function

---
+++ backend/app/services/user_service.py
@@ -0,0 +1,28 @@
+from app.models.user import User
+from app import db
+from app.extensions import bcrypt
+
+class UserService():
+
+    @staticmethod
+    def create_user(data):
+        # Check if email already exists
+        existing_email = User.query.filter_by(email=data['email'].first())
+        if existing_email:
+            raise ValueError("Email already registered")
+
+        # User instance
+        user = User(
+            first_name=data['first_name'],
+            last_name=data["last_name"],
+            email=data["email"],
+            image_url=data.get("image_url")
+        )
+
+        # Model handles hashing of ps
+        user.set_password(data['password'])
+
+        db.session.add(user)
+        db.session.commit()
+
+        return user
