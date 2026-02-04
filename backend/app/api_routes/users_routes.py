ad1169aded10fed2b08153aaee6d250bc68cc86e
Emily Chew <emilychewsh@gmail.com>
2026-02-04 13:00:10 +1100

    remove get all users method, add POST for user creation, tested

--- backend/app/api_routes/users_routes.py
+++ backend/app/api_routes/users_routes.py
@@ -1,30 +1,43 @@
 from flask import Blueprint, jsonify, request
-from app.models.user import User
-from app.extensions import db
+from app.services.user_service import UserService
 
 users_bp = Blueprint('users', __name__, url_prefix='/api/users')
-
-@users_bp.route('/', methods=['GET'])
-def get_all_users():
-    """ Fetch all users """
-    users = User.query.all()
-    return jsonify([
-        {
-            'first_name': u.first_name,
-            'last_name': u.last_name,
-            'email': u.email
-        }
-        for u in users
-    ])
 
 @users_bp.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     """ Fetch a single user by ID """
-    user = User.query.get_or_404(user_id) # return 404 if not found
-    return jsonify([
-           {
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'email': user.email
-            }
-    ])
+    try:
+        user = UserService.get_user_by_id(user_id)        
+        return jsonify({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "image_url": user.image_url,
+            "role": user.role
+        }), 200
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 404
+
+@users_bp.route('/', methods=['POST'])
+def create_user():
+    """ Create a new user """
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON"}), 400
+    
+    data = request.get_json()
+
+    required_fields = {"first_name", "last_name", "email", "password"}
+    if not required_fields:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    try:
+        user = UserService.create_user(data)
+        return jsonify({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+        }), 201
+    except ValueError as e:
+        return jsonify({"error: str(e)"}), 400
