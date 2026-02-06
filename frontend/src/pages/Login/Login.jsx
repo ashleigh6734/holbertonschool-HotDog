8e85b0c2fb1b6cfdf55cf10e9654a038a3838522
“cryspycreme” <11529@holbertonstudents.com>
2026-02-06 14:22:34 +1100

    feat/ Apply background image to auth pages only

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -6,43 +6,39 @@ import ToggleSwitch from "../../components/buttons/ToggleSwitch";
 
 export default function Login() {
   return (
-    <div className="form-card">
-      <Form>
-        <ToggleSwitch
-          firstRadio={"Login"}
-          firstPath={"/login"}
-          secondRadio={"Sign Up"}
-          secondPath={"/register"}
-        />
-
-        <h2>Login</h2>
-
-        <p className="mb-3 sub-text">Access your dashboard now! </p>
-
-        <FormLabel
-          className="justify-left mb-3"
-          controlId="loginEmail"
-          type="email"
-          placeholder="Email"
-          name="Email"
-        />
-
-        <FormLabel
-          className="justify-left mb-4"
-          controlId="loginPassword"
-          type="password"
-          placeholder="Password"
-          name="Password"
-        />
-
-        <Button className="login-btn" variant="warning" type="logIn">
-          Login
-        </Button>
-
-        <p className="sub-text justify-left">
-          New to HotDog? <a href="../register">Sign up</a>
-        </p>
-      </Form>
+    <div className="auth-page">
+      <div className="form-card">
+        <Form>
+          <ToggleSwitch
+            firstRadio={"Login"}
+            firstPath={"/login"}
+            secondRadio={"Sign Up"}
+            secondPath={"/register"}
+          />
+          <h2>Login</h2>
+          <p className="mb-3 sub-text">Access your dashboard now! </p>
+          <FormLabel
+            className="justify-left mb-3"
+            controlId="loginEmail"
+            type="email"
+            placeholder="Email"
+            name="Email"
+          />
+          <FormLabel
+            className="justify-left mb-4"
+            controlId="loginPassword"
+            type="password"
+            placeholder="Password"
+            name="Password"
+          />
+          <Button className="login-btn" variant="warning" type="logIn">
+            Login
+          </Button>
+          <p className="sub-text justify-left">
+            New to HotDog? <a href="../register">Sign up</a>
+          </p>
+        </Form>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -6,80 +6,72 @@ import ToggleSwitch from "../../components/buttons/ToggleSwitch";
 
 export default function Register() {
   return (
-    <div className="form-card">
-      <Form>
-        <ToggleSwitch
-          firstRadio={"Login"}
-          firstPath={"/login"}
-          secondRadio={"Sign Up"}
-          secondPath={"/register"}
-        />
-
-        <h2>Create a HotDog Account</h2>
-
-        <p className="mb-5 sub-text">
-          Keep on top of your pet's appointments and create an account!{" "}
-        </p>
-
-        <Row className="mb-3">
-          <FormLabel
-            className="justify-left"
-            controlId="formGridFirstName"
-            type="firstname"
-            placeholder="First Name"
-            name="First Name*"
+    <div className="auth-page">
+      <div className="form-card">
+        <Form>
+          <ToggleSwitch
+            firstRadio={"Login"}
+            firstPath={"/login"}
+            secondRadio={"Sign Up"}
+            secondPath={"/register"}
           />
-
+          <h2>Create a HotDog Account</h2>
+          <p className="mb-5 sub-text">
+            Keep on top of your pet's appointments and create an account!{" "}
+          </p>
+          <Row className="mb-3">
+            <FormLabel
+              className="justify-left"
+              controlId="formGridFirstName"
+              type="firstname"
+              placeholder="First Name"
+              name="First Name*"
+            />
+            <FormLabel
+              className="justify-left"
+              controlId="formGridLastName"
+              type="lastname"
+              placeholder="Last Name"
+              name="Last Name*"
+            />
+          </Row>
           <FormLabel
-            className="justify-left"
-            controlId="formGridLastName"
-            type="lastname"
-            placeholder="Last Name"
-            name="Last Name*"
+            className="justify-left mb-3"
+            controlId="formMobileNumber"
+            type="mobilenumber"
+            placeholder="Mobile Number"
+            name="Mobile Number"
           />
-        </Row>
-
-        <FormLabel
-          className="justify-left mb-3"
-          controlId="formMobileNumber"
-          type="mobilenumber"
-          placeholder="Mobile Number"
-          name="Mobile Number"
-        />
-
-        <FormLabel
-          className="justify-left mb-3"
-          controlId="formEmail"
-          type="email"
-          placeholder="Email"
-          name="Email*"
-        />
-
-        <FormLabel
-          className="justify-left mb-3"
-          controlId="formPassword"
-          type="password"
-          placeholder="Enter Password"
-          name="Password*"
-        />
-
-        <FormLabel
-          className="justify-left mb-3"
-          controlId="formConfirmPassword"
-          type="password"
-          placeholder="Re-enter Password"
-          name="Confirm Password*"
-        />
-
-        <Button className="signup-btn" variant="warning" type="signUp">
-          Sign Up
-        </Button>
-
-        <p className="sub-text justify-left">
-          {" "}
-          Already have an account? <a href="../login">Login</a>
-        </p>
-      </Form>
+          <FormLabel
+            className="justify-left mb-3"
+            controlId="formEmail"
+            type="email"
+            placeholder="Email"
+            name="Email*"
+          />
+          <FormLabel
+            className="justify-left mb-3"
+            controlId="formPassword"
+            type="password"
+            placeholder="Enter Password"
+            name="Password*"
+          />
+          <FormLabel
+            className="justify-left mb-3"
+            controlId="formConfirmPassword"
+            type="password"
+            placeholder="Re-enter Password"
+            name="Confirm Password*"
+          />
+          <Button className="signup-btn" variant="warning" type="signUp">
+            Sign Up
+          </Button>
+          <p className="sub-text justify-left">
+            {" "}
+            Already have an account? <a href="../login">Login</a>
+          </p>
+        </Form>
+      </div>
     </div>
   );
 }
