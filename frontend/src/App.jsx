898ce29f0d6bc461d9e6f040013c870f2b95fad2
“cryspycreme” <11529@holbertonstudents.com>
2026-02-12 13:55:05 +1100

    draft: create appointment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
         indiv pet
         <Route path="/pets" element={<AllPets />} /> // all pets
         <Route path="/user" element={<UserProfile />} />
-        <Route path="/appointments/:id" element={<Appointments />} />
+        <Route path="/appointments" element={<Appointments />} />
         {/* more pages coming ... */}
       </Routes>
 
--- frontend/src/pages/Appointments/Appointments.jsx
+++ frontend/src/pages/Appointments/Appointments.jsx
@@ -1,8 +1,46 @@
+import "./Appointments.css";
+
 export default function Appointments() {
   return (
-    <div>
-      <h1>Appointments</h1>
-      <p>Welcome to your appointments page!</p>
+    <div className="appointment-page">
+      <div className="appointment-container">
+        <div className="provider-content">
+          <h1>All Things Pets Clinic</h1>
+          <div className="provider-img-container">
+            <img
+              src="src/assets/images/vet-clinic-1.jpg"
+              alt="provider-image"
+            />
+          </div>
+
+          <div className="provider-info">
+            <div className="provider-address flex-gap">
+              <img src="src/assets/icons/geo-alt.svg" alt="Bootstrap" />
+              <span>12 South Street, Melbourne 3000</span>
+            </div>
+            <div className="provider-email flex-gap">
+              <img src="src/assets/icons/email.svg" alt="Bootstrap" />
+              <span> allthingspetsclinic@businessemail.com</span>
+            </div>
+            <div className="provider-number flex-gap">
+              <img src="src/assets/icons/phone.svg" alt="Bootstrap" />
+              <span>0427 272 783</span>
+            </div>
+          </div>
+          <div className="provider-details">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse
+          </div>
+        </div>
+        <div className="bookings">
+          <div className="bookings-banner">Make a booking</div>
+        </div>
+        /* PLACEHOLDER FOR REVIEWS */
+      </div>
     </div>
   );
 }
