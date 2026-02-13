8ffe7ba7be8bfd3f3a501309ff54ab4b487237d6
Emily <150105586+by-emrii@users.noreply.github.com>
2026-02-13 13:53:17 +1100

    Update Register.jsx to include state, Update FormLabel and gitigore backend

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/users";
+const API_URL = "http://localhost:5000/api/users/";
 
 export async function registerUser(userData) {
     const response = await fetch(`${API_URL}`, {
