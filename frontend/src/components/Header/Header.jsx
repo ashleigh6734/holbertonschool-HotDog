f1c502db6598e6ecc950f54248aee66ce06053f3
GraceKayem <11557@holbertonstudents.com>
2026-02-18 12:56:59 +1100

    renames logoHeader to blueLogo for more precision

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,28 +3,19 @@ import GuestHeader from "./GuestHeader";
 import UserHeader from "./UserHeader";
 import Avatar from "../avatar/Avatar.jsx";
 import "./Header.css";
-import logoHeader from "../../assets/logo/hotdog_logo_blue_background.svg";
 
-//images
-import logoHeader from "../../assets/logo/hotdog_logo_yellow background.svg";
-import profileIcon from "../../assets/icons/account-profile.png";
+import blueLogo from "../../assets/logo/hotdog_logo_blue_background.svg";
 
 function Header({ isLoggedIn }) {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <header> 
-      {/* Logo */}
-      <img 
-        src={logoHeader}
-        alt="HotDog Logo" 
-        className="hotdog-logo"
-      />
+    <header>
+      <img src={blueLogo} alt="HotDog Logo" className="hotdog-logo" />
 
       {/* Navigation */}
       <nav>{isLoggedIn ? <UserHeader /> : <GuestHeader />}</nav>
 
       {/* EMILY, I'M HERE*/}
-
       {isLoggedIn ? (
         <Avatar />
       ) : (
