d2222f130f5e0072b1c09e5434f3a5ced841be88
GraceKayem <11557@holbertonstudents.com>
2026-02-18 14:30:56 +1100

    truned images into imports

--- frontend/src/pages/Home/card_Data.js
+++ frontend/src/pages/Home/card_Data.js
@@ -1,22 +1,27 @@
+import VetConsultationsImage from "../../assets/images/2149314374.jpg";
+import DesexingImage from "../../assets/images/portrait-happy-couple-home-with-dog.jpg";
+import DentalCare from "../../assets/images/vet-is-checking-cat-s-teeth.jpg";
+import NailTrimingImage from "../../assets/images/51531.jpg";
+
 const cardDetails = [
   {
     id: 1,
-    img: "../../assets/images/2149314374.jpg",
+    img: VetConsultationsImage,
     title: "Vet Consultations"
   },
   {
     id: 2,
-    img: "../../assets/images/portrait-happy-couple-home-with-dog.jpg",
+    img: DesexingImage,
     title: "Desexing & Surgical Procedures"
   },
   {
     id: 3,
-    img: "../../assets/images/vet-is-checking-cat-s-teeth.jpg",
+    img: DentalCare,
     title: "Dental Care"
   },
   {
     id: 6,
-    img: "../../assets/images/51531.jpg",
+    img: NailTrimingImage,
     title: "Nail Trimming"
   },
 
