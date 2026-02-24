bef3c259b55116838e7d796d0672f7372d398a2b
“cryspycreme” <11529@holbertonstudents.com>
2026-02-24 17:13:47 +1100

    feat: style ProviderSide Appointments page

--- frontend/src/pages/ProviderPages/ProviderBookings/ProviderBookings.jsx
+++ frontend/src/pages/ProviderPages/ProviderBookings/ProviderBookings.jsx
@@ -2,11 +2,17 @@ import "./ProviderBookings.css";
 import UpcomingEvents from "../../../components/ProviderSideAppointments/UpcomingEvents";
 import { UPCOMING_APPOINTMENTS } from "./ProviderBookingsData";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import dayjs from "dayjs";
 import DateStep from "../../Appointments/DateStep";
 import TimeStep from "../../Appointments/TimeStep";
 
 export default function ProviderBookings() {
   const navigate = useNavigate();
+  const today = dayjs();
+  const [selectedDate, setSelectedDate] = useState(today); // store selected date
+  const [availableTimes, setAvailableTimes] = useState([]);
+  const [selectedTime, setSelectedTime] = useState("");
   return (
     <div className="wrapper-container">
       <div className="bookings-header-bar">
@@ -25,17 +31,46 @@ export default function ProviderBookings() {
           <UpcomingEvents upcomingEvents={UPCOMING_APPOINTMENTS} />
         </div>
       </div>
-      {/* <div className="bookings-container"> */}
-      <div className="provider-date-container">
-        <DateStep
-          sx={{
-            transform: "scale(1.5)",
-            transformOrigin: "top left",
-          }}
-        />
+      <div className="new-appointment-container">
+        <div className="book-appointment-banner">
+          <p>Book a new appointment</p>
+        </div>
+        <div className="provider-bookings-container">
+          <div className="date-container">
+            <DateStep
+              value={selectedDate}
+              onChange={setSelectedDate}
+              sx={{
+                margin: 0,
+                padding: 0,
+                transform: "scale(1.2)",
+                alignSelf: "flex-start",
+              }}
+            />
+          </div>
+          <div className="time-container">
+            <TimeStep
+              selectedTime={selectedTime}
+              setSelectedTime={setSelectedTime}
+              times={availableTimes}
+            />
+          </div>
+        </div>
+
+        {selectedTime != "" && (
+          <div className="action-btn-container">
+            <button
+              className="action-btn-format grey-btn"
+              onClick={() => {
+                setSelectedTime("");
+              }}
+            >
+              Cancel
+            </button>
+            <button className="action-btn-format navy-btn">Book</button>
+          </div>
+        )}
       </div>
-      <div className="time-container">{/* <TimeStep /> */}</div>
     </div>
-    // </div>
   );
 }
